b8a21089f15953d9619458269598466138478bb9
Jimmyjoe13 <jimmyjoe13@gmail.com>
2025-02-15 23:27:43 +0100

    fix: update dependencies and Replit configuration for Python 3.10

--- replit.nix
+++ replit.nix
@@ -1,10 +1,12 @@
-{ pkgs }: {
-    deps = [
-        pkgs.python310
-        pkgs.python310Packages.pip
-        pkgs.python310Packages.setuptools
-        pkgs.python310Packages.wheel
-        pkgs.gcc
-        pkgs.swig
-    ];
+{
+  description = "Python environment for IT-Bot";
+  deps = [
+    pkgs.python310
+    pkgs.python310Packages.pip
+    pkgs.gcc
+    pkgs.swig
+    pkgs.pkg-config
+    pkgs.openssl
+    pkgs.git
+  ];
 }
